d103df8be7d76bd969678ddfd06a9ede28f4c5af
정현욱 <junghunwook456@naver.com>
2023-11-18 18:10:35 +0900

    firebase

---
+++ nwitter-reloaded/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHb03V-OV7Ti-oUEL4QajlQzFzyIae4gY",
+  authDomain: "nwitter-reloaded-942d4.firebaseapp.com",
+  projectId: "nwitter-reloaded-942d4",
+  storageBucket: "nwitter-reloaded-942d4.appspot.com",
+  messagingSenderId: "315046805881",
+  appId: "1:315046805881:web:58da6d81cf5f561429afa8"
+};
+
+const app = initializeApp(firebaseConfig);
